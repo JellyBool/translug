d818226cae5f044e3801826102ee181628e70763
JellyBool <jellybool@outlook.com>
2016-09-19 03:46:44 +0800

    add more unit tets

--- src/Translug.php
+++ src/Translug.php
@@ -60,15 +60,12 @@ class Translug
      */
     private function sluggable($title, $separator = '-')
     {
-        // Convert all dashes/underscores into separator
         $flip = $separator == '-' ? '_' : '-';
 
         $title = preg_replace('![' . preg_quote($flip) . ']+!u', $separator, $title);
 
-        // Remove all characters that are not the separator, letters, numbers, or whitespace.
         $title = preg_replace('![^' . preg_quote($separator) . '\pL\pN\s]+!u', '', mb_strtolower($title));
 
-        // Replace all separator characters and whitespace by a single separator
         $title = preg_replace('![' . preg_quote($separator) . '\s]+!u', $separator, $title);
 
         return trim($title, $separator);
--- tests/TranslugTest.php
+++ tests/TranslugTest.php
@@ -4,25 +4,44 @@ class TranslugTest extends PHPUnit_Framework_TestCase
 {
     protected $translug;
 
+    protected $translator;
+
     public function setUp()
     {
-        $this->translug = new \JellyBool\Translug\Translation(new \GuzzleHttp\Client());
+        $this->translator = new \JellyBool\Translug\Translation(new \GuzzleHttp\Client());
+        $this->translug = new JellyBool\Translug\Translug();
     }
 
     /** @test */
-    public function it_check_the_english_title_translated_text()
+    public function it_test_the_english_title_translated_text()
     {
-        $english = $this->translug->translate('How to install Laravel');
+        $translatedText = $this->translator->translate('How to install Laravel');
 
-        $this->assertEquals('How to install Laravel',$english);
+        $this->assertEquals('How to install Laravel',$translatedText);
     }
 
     /** @test */
-    public function it_check_the_english_title_slug_text()
+    public function it_test_the_english_title_slug_text()
     {
-        $english = $this->translug->translug('How to install Laravel');
+        $slugText = $this->translator->translug('How to install Laravel');
 
-        $this->assertEquals('how-to-install-laravel',$english);
+        $this->assertEquals('how-to-install-laravel',$slugText);
+    }
+
+    /** @test */
+    public function it_test_the_english_title_translated_text_with_slug_class()
+    {
+        $tranlatedText = $this->translug->translate('How to install Laravel');
+
+        $this->assertEquals('How to install Laravel',$tranlatedText);
+    }
+
+    /** @test */
+    public function it_test_the_english_title_slug_text_with_slug_class()
+    {
+        $slugText = $this->translug->translug('How to install Laravel');
+
+        $this->assertEquals('how-to-install-laravel',$slugText);
     }
 
 }
